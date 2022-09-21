99c75706d835e0955de03f572bb1e59bda195f18
Ankush Gupta <ankushgupta365@gmail.com>
2022-09-21 13:27:36 +0530

    Product schema created

---
+++ models/Product.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+})
+
+exports.module = mongoose.model('Product', ProductSchema)
