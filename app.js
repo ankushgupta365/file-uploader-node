ea20242dff22febd3be1e914563b95064eb86925
Ankush Gupta <ankushgupta365@gmail.com>
2022-09-22 04:00:09 +0530

    security packages added and heroku environvent setted up

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ require('express-async-errors');
 
 const express = require('express');
 const app = express();
+//extra security pacakages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
 const fileUploader = require('express-fileupload')
 const cloudinary = require('cloudinary').v2
 cloudinary.config({
@@ -10,6 +15,7 @@ cloudinary.config({
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET
 })
+
 // database
 const connectDB = require('./db/connect');
 
@@ -18,7 +24,16 @@ const productRouter = require('./routes/productRoutes')
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
-
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 app.use(express.static('./public'))
 app.use(express.json())
 app.use(fileUploader({useTempFiles: true}))
